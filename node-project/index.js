1bc92ad56d5db2769c177e71af0273f6e7ac564f
headchezze <63397164+headchezze@users.noreply.github.com>
2022-05-10 16:22:58 +0500

    Первичный коммит

---
+++ node-project/index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const config = require("config");
+const mongoose = require("mongoose");
+
+
+const app = express();
+const PORT = config.get("port");
+
+async function connectToDB()
+{
+    try
+    {
+       await mongoose.connect(config.get("mongoUri"), {
+        userNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateInxed: true
+       });
+       app.listen(PORT, () => console.log("App has been started"));
+    } catch(e)  
+    {
+        console.log("Server Error", e.message);
+        process.exit(1);
+    }
+}
+
+connectToDB();
+
