4437073ce1772601e3abe017a320863406314044
headchezze <63397164+headchezze@users.noreply.github.com>
2022-05-11 14:35:14 +0500

    Логика регистрации пользователя

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,10 +1,26 @@
-const {Router} = require("express");
+const {Router} = require("express")
+const bcrypt = require('bcryptjs')
+const {check, validationResult} = require('express-validator')
 const User = require('../models/User')
 const router = Router();
 
-router.post('/register', async (req, res) => {
+router.post(
+    '/register',
+    [
+        check('email', 'Incorrect email').isEmail(),
+        check('password', 'Password must have at least 6 characters').isLength({min: 6})
+    ],
+    async (req, res) => {
     try
     {
+        const errors = validationResult(req)
+
+        if(!errros.isEmpty())
+        {
+            return res.status(400).json({errors: errors.array(),
+            message: 'Invalid data'})
+        }
+
         const {email, password} = req.body;
 
         const candidate = await User.findOne({email: email});
@@ -14,7 +30,12 @@ router.post('/register', async (req, res) => {
             return res.status(400).json({message: 'Such a user already exists'});
         }
 
-        
+        const hashedPassword = await bcrypt.hash(password, 23)
+        const user = new User({email: email, password: hashedPassword})
+
+        await user.save()
+
+        res.status(201).json({message: 'User has been created'})
 
     } catch(e)
     {
