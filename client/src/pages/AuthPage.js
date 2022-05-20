b8ba56972af493d3457c670260f46bdddf39ad2e
Quildy <81456797+Quildy@users.noreply.github.com>
2022-05-20 13:28:55 +0500

    Работа с отображением главной формы, внешним видом

--- client/src/pages/AuthPage.js
+++ client/src/pages/AuthPage.js
@@ -1,9 +1,53 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 export const AuthPage = () => {
+    const [form, setForm] = useState({
+        email: '', password: ''
+    })
+    
+    const changeHandler = event => {
+        setForm({...form, [event.target.name]: event.target.value })
+    }
+    
     return (
-        <div>
-            <h1>Auth Page</h1>
+        <div className = "row">
+            <div className="col s6 offset-s3">
+                <h1>Сокращение ссылок</h1>
+                <div className="card light-blue darken-3">
+                    <div className="card-content white-text">
+                        <span className="card-title" style={{marginLeft: 205}}>Авторизация</span>
+                        <div>
+                            <div className="input-field">
+                                <input placeholder="" 
+                                       id="email"
+                                       type="text"
+                                       name="email"
+                                       className="color-input"
+                                       onChange={changeHandler}
+                                       />
+                                    <label htmlFor="email">Введите почту</label>
+                            </div>
+
+                            <div className="input-field">
+                                <input placeholder=""
+                                       id="password"
+                                       type="password"
+                                       name= "password"
+                                       className="color-input"
+                                       onChange={changeHandler}
+                                />
+                                <label htmlFor="email">Введите пароль</label>
+                            </div>
+                            
+                        </div>
+                    </div>
+                    <div className="card-action">
+                        <button className="btn ldeep-purple lighten-3" style={{marginLeft: 85, marginRight: 30}}>Войти в систему</button>
+                        <button className="btn deep-orange lighten-1">Зарегистрироваться</button>
+                    </div>
+                </div>
+                <h8>Учебный проект по ПИ</h8>
+            </div>
         </div>
     )
 }
