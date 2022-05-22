e6e81f0815bf5000aec40383960d0d00616c614f
headchezze <gribnik2281337@gmail.com>
2022-05-22 23:15:57 +0500

    фиксация "test"

--- client/src/App.js
+++ client/src/App.js
@@ -2,16 +2,24 @@ import React from 'react';
 import {BrowserRouter as Router} from 'react-router-dom'
 import {useRoutes} from './routes' 
 import 'materialize-css'
+import { useAuth } from './hooks/auth.hook';
+import { AuthContext } from './context/AuthContext';
 
 
 function App() {
-    const routes = useRoutes(false)
+    const {token, login, logout, userId} = useAuth()
+    const isAuth = !!token
+    const routes = useRoutes(isAuth)
   return (
+      <AuthContext.Provider value={{
+          token, login, logout, userId, isAuth
+      }}>
       <Router>
           <div className="container">
               {routes}
           </div>
       </Router>
+      </AuthContext.Provider>
   )
 }
 
--- client/src/pages/AuthPage.js
+++ client/src/pages/AuthPage.js
@@ -1,14 +1,44 @@
-import React, {useState} from 'react'
+import React, {useContext, useEffect, useState} from 'react'
+import { AuthContext } from '../context/AuthContext'
+import { useHttp } from '../hooks/http.hook'
 
 export const AuthPage = () => {
+    const auth = useContext(AuthContext)
+    const message = useMessage()
+    const {loading, request, error, clearError} = useHttp()
     const [form, setForm] = useState({
         email: '', password: ''
     })
+
+    useEffect(() => {
+        message(error)
+        clearError()
+    }, [error, message, clearError])
     
     const changeHandler = event => {
         setForm({...form, [event.target.name]: event.target.value })
     }
     
+    const registerHandler = async () =>{
+        try {
+            const data = await request('/api/auth/register', 'POST', {...form})
+            auth.login(data.token, data.userId)
+        } catch (error) {
+            
+        }
+
+    }
+
+    const loginHandler = async () =>{
+        try {
+            const data = await request('/api/auth/login', 'POST', {...form})
+            message(data.message)
+        } catch (error) {
+            
+        }
+
+    }
+
     return (
         <div className = "row">
             <div className="col s6 offset-s3">
@@ -42,8 +72,17 @@ export const AuthPage = () => {
                         </div>
                     </div>
                     <div className="card-action">
-                        <button className="btn ldeep-purple lighten-3" style={{marginLeft: 85, marginRight: 30}}>Войти в систему</button>
-                        <button className="btn deep-orange lighten-1">Зарегистрироваться</button>
+                        <button 
+                        className="btn ldeep-purple lighten-3"  
+                        style={{marginLeft: 85, marginRight: 30}}
+                        onClick={loginHandler}
+                        disabled={loading}>
+                        Войти в систему</button>
+                        <button 
+                        className="btn deep-orange lighten-1"
+                        onClick={registerHandler}
+                        disabled={loading}>
+                        Зарегистрироваться</button>
                     </div>
                 </div>
                 <h8>Учебный проект по ПИ</h8>
