53a38b029f629c5a9d2c42dab7f1d39a00d02b2a
walty-collab <74611055+walty-collab@users.noreply.github.com>
2022-05-31 01:53:43 +0500

    добавлены 2 LinkCard и Loader

---
+++ client/src/components/LinkCard.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const LinkCard = ({ link}) => {
+    return(
+        <>
+            <h2> Сыллка </h2>
+            
+            <p>Ваша ссылка: <a href={link.to} target="_blank" rel="noopener noreferrer"> {link.to}</a></p>
+            <p>Откуда: <a href={link.from} target="_blank" rel="noopener noreferrer"> {link.from}</a></p>
+            <p>Количество кликов по ссылке: <strong>{link.click}</strong></p>
+        <p>Дата создания: <string>{new Date(link.date).toLocaleDateString()}</string></p>
+        </>
+    )
+}
