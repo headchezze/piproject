f08f5461782124058f9bb720e9eb2feda2cdc5c0
headchezze <gribnik2281337@gmail.com>
2022-06-05 20:57:18 +0500

    fix

--- client/src/components/LinkCard.js
+++ client/src/components/LinkCard.js
@@ -7,7 +7,7 @@ export const LinkCard = ({ link}) => {
             
             <p>Ваша ссылка: <a href={link.to} target="_blank" rel="noopener noreferrer"> {link.to}</a></p>
             <p>Откуда: <a href={link.from} target="_blank" rel="noopener noreferrer"> {link.from}</a></p>
-            <p>Количество кликов по ссылке: <strong>{link.click}</strong></p>
+            <p>Количество кликов по ссылке: <strong>{link.clicks}</strong></p>
         <p>Дата создания: <string>{new Date(link.date).toLocaleDateString()}</string></p>
         </>
     )
